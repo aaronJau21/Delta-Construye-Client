9989232c2581af3c3a4c5a68e9acb64db0459ba0
Aaronjau21 <aaronjau21@gmail.com>
2025-03-23 17:51:34 -0500

    Cambios en terminos y condiciones

--- components/ui/footer/FooterComponent.vue
+++ components/ui/footer/FooterComponent.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="bg-[#D9D9D9] text-center px-6 sm:px-8 md:px-12">
     <div class="w-full max-w-screen-lg mx-auto py-16">
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-20 text-center md:text-left items-start">
+      <div
+        class="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-20 text-center md:text-left items-start"
+      >
         <!-- Logo y descripción -->
         <div class="flex flex-col items-center md:items-start">
           <img src="/public/delta-logo.png" alt="Logo" class="w-36 mb-3" />
-          <p class="text-gray-600">Somos el marketplace B2B de la construcción en Perú</p>
+          <p class="text-gray-600">
+            Somos el marketplace B2B de la construcción en Perú
+          </p>
           <span class="text-gray-500">Razón Social: EJEMPLO S.A.C.</span>
           <span class="text-gray-500">RUC: **********</span>
         </div>
@@ -13,15 +17,21 @@
         <!-- Servicio al Cliente -->
         <div class="flex flex-col gap-2">
           <h3 class="font-bold text-2xl mb-4">Servicio al Cliente</h3>
-          <NuxtLink to="/privacy-policy" class="text-gray-600 hover:text-red-600 cursor-pointer block border-none">
+          <NuxtLink
+            to="/privacy-policy"
+            class="text-gray-600 hover:text-red-600 cursor-pointer block border-none"
+          >
             Política de Privacidad
           </NuxtLink>
-          <NuxtLink to="/terms-and-conditions" class="text-gray-600 hover:text-red-600 cursor-pointer block">
+          <NuxtLink
+            to="/terms-and-conditions"
+            class="text-gray-600 hover:text-red-600 cursor-pointer block"
+          >
             Terminos y Condiciones
           </NuxtLink>
-          <NuxtLink to="/complaints-book" class="text-gray-600 hover:text-red-600 cursor-pointer block border-none">
+          <!-- <NuxtLink to="/complaints-book" class="text-gray-600 hover:text-red-600 cursor-pointer block border-none">
             Libro de Reclamaciones
-          </NuxtLink>
+          </NuxtLink> -->
         </div>
 
         <!-- Contacto -->
@@ -35,9 +45,18 @@
       <!-- Redes Sociales -->
       <div class="mt-10">
         <div class="flex justify-center space-x-4 mb-6">
-          <Icon name="fa:facebook-official" class="text-blue-600 text-3xl hover:text-blue-800" />
-          <Icon name="fa:instagram" class="text-pink-600 text-3xl hover:text-pink-800" />
-          <Icon name="fa:linkedin-square" class="text-blue-600 text-3xl hover:text-blue-800" />
+          <Icon
+            name="fa:facebook-official"
+            class="text-blue-600 text-3xl hover:text-blue-800"
+          />
+          <Icon
+            name="fa:instagram"
+            class="text-pink-600 text-3xl hover:text-pink-800"
+          />
+          <Icon
+            name="fa:linkedin-square"
+            class="text-blue-600 text-3xl hover:text-blue-800"
+          />
         </div>
         <p class="text-gray-500 text-sm">
           © DeltaConstruye | Todos los Derechos Reservados.
--- pages/panel/perfil/index.vue
+++ pages/panel/perfil/index.vue
@@ -1,14 +1,19 @@
 <template>
-<div class="flex h-full bg-gray-100" style="min-height: calc(100vh - 80px);">
-
+  <div class="flex h-full bg-gray-100" style="min-height: calc(100vh - 80px)">
     <!-- Barra lateral -->
-    <aside class="w-[300px] bg-white shadow-md p-6 flex flex-col items-center border ">
+    <aside
+      class="w-[300px] bg-white shadow-md p-6 flex flex-col items-center border"
+    >
       <div class="flex flex-col items-center w-full">
         <!-- Imagen de perfil -->
         <label for="imageInput" class="relative cursor-pointer group">
-          <div class="flex justify-center rounded-full overflow-hidden border-2 border-gray-300">
+          <div
+            class="flex justify-center rounded-full overflow-hidden border-2 border-gray-300"
+          >
             <label for="imageInput">
-              <div class="rounded-full flex items-center justify-center bg-white">
+              <div
+                class="rounded-full flex items-center justify-center bg-white"
+              >
                 <img
                   v-if="imageSrc"
                   class="w-32 h-32 rounded-full cursor-pointer object-cover"
@@ -37,28 +42,36 @@
       <!-- Opciones de cuenta -->
       <nav class="mt-6 w-full">
         <ul class="space-y-2">
-          <li class="p-2 bg-gray-200 rounded-lg font-semibold flex items-center justify-between">
+          <li
+            class="p-2 bg-gray-200 rounded-lg font-semibold flex items-center justify-between"
+          >
             <div class="flex items-center gap-2">
               <Icon name="mdi:account" class="text-gray-600 text-xl" />
               Cuenta
             </div>
             <Icon name="mdi:chevron-right" class="text-gray-400 text-xl" />
           </li>
-          <li class="p-2 hover:bg-gray-100 rounded-lg cursor-pointer flex items-center justify-between">
+          <li
+            class="p-2 hover:bg-gray-100 rounded-lg cursor-pointer flex items-center justify-between"
+          >
             <div class="flex items-center gap-2">
               <Icon name="mdi:lock" class="text-gray-600 text-xl" />
               Cambiar la contraseña
             </div>
             <Icon name="mdi:chevron-right" class="text-gray-400 text-xl" />
           </li>
-          <li class="p-2 hover:bg-gray-100 rounded-lg cursor-pointer flex items-center justify-between">
+          <li
+            class="p-2 hover:bg-gray-100 rounded-lg cursor-pointer flex items-center justify-between"
+          >
             <div class="flex items-center gap-2">
               <Icon name="mdi:shield-lock" class="text-gray-600 text-xl" />
               Privacidad
             </div>
             <Icon name="mdi:chevron-right" class="text-gray-400 text-xl" />
           </li>
-          <li class="p-2 hover:bg-gray-100 rounded-lg cursor-pointer text-red-500 flex items-center justify-between">
+          <li
+            class="p-2 hover:bg-gray-100 rounded-lg cursor-pointer text-red-500 flex items-center justify-between"
+          >
             <div class="flex items-center gap-2">
               <Icon name="mdi:delete" class="text-red-500 text-xl" />
               Borrar la cuenta
@@ -67,7 +80,6 @@
           </li>
         </ul>
       </nav>
-
     </aside>
 
     <!-- Área principal -->
@@ -77,21 +89,40 @@
 
       <!-- Contenedor que centra el formulario en X y Y -->
       <div class="flex flex-1 items-center justify-center mb-40">
-        <form @submit.prevent="updateUser" class="bg-white shadow-md rounded-lg p-6 max-w-lg w-full flex flex-col">
+        <form
+          @submit.prevent="updateUser"
+          class="bg-white shadow-md rounded-lg p-6 max-w-lg w-full flex flex-col"
+        >
           <div class="mb-4">
             <label class="block text-gray-600 font-semibold">Nombre:</label>
-            <input type="text" v-model="dataUpdateUser.name" class="border border-gray-300 rounded p-2 w-full" />
+            <input
+              type="text"
+              v-model="dataUpdateUser.name"
+              class="border border-gray-300 rounded p-2 w-full"
+            />
           </div>
           <div class="mb-4">
-            <label class="block text-gray-600 font-semibold">Dirección de correo electrónico:</label>
-            <input type="email" v-model="dataUpdateUser.email" class="border border-gray-300 rounded p-2 w-full" />
+            <label class="block text-gray-600 font-semibold"
+              >Dirección de correo electrónico:</label
+            >
+            <input
+              type="email"
+              v-model="dataUpdateUser.email"
+              class="border border-gray-300 rounded p-2 w-full"
+            />
           </div>
           <div class="mb-4">
             <label class="block text-gray-600 font-semibold">Teléfono:</label>
-            <input type="text" v-model="dataUpdateUser.phone" class="border border-gray-300 rounded p-2 w-full" />
+            <input
+              type="text"
+              v-model="dataUpdateUser.phone"
+              class="border border-gray-300 rounded p-2 w-full"
+            />
           </div>
           <div class="flex justify-center mt-4">
-            <button class="bg-blue-500 text-white px-4 py-2 rounded-lg text-lg font-semibold">
+            <button
+              class="bg-blue-500 text-white px-4 py-2 rounded-lg text-lg font-semibold"
+            >
               Actualizar la cuenta
             </button>
           </div>
@@ -100,8 +131,6 @@
     </div>
   </div>
 </template>
-
-
 
 <script setup lang="ts">
 definePageMeta({
